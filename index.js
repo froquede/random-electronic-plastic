b49c871811f2741aafb1a3c6998c966e53055dbb
Roque Francisco <roquefsd@gmail.com>
2021-02-17 23:14:03 -0300

    update paths

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-import { html, render } from '/web_modules/lit-html.js';
-import '/components/entry.js';
+import { html, render } from './web_modules/lit-html.js';
+import './components/entry.js';
 
 render(html`
     <style>
